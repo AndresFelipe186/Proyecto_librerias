756d0d5b03868e560b5dcf80ae3ef6b39c44c1c0
unknown <andrescastro988@gmail.com>
2020-07-27 16:01:56 -0500

    Se realiza codigo app.js el primer punto cambio de color del titutlo

---
+++ js/app.js
@@ -0,0 +1,38 @@
+//se realiza cambio del color en el titulo que prenda y apague 
+function colorBlink(selector) {
+	$(selector).animate({
+			opacity: '1',
+		}, {
+			step: function () {
+				$(this).css('color', 'white');
+			},
+			queue: true
+		})
+		.animate({
+			opacity: '1'
+		}, {
+			step: function () {
+				$(this).css('color', 'yellow');
+			},
+			queue: true
+		}, 600)
+		.delay(1000)
+		.animate({
+			opacity: '1'
+		}, {
+			step: function () {
+				$(this).css('color', 'white');
+			},
+			queue: true
+		})
+		.animate({
+			opacity: '1'
+		}, {
+			step: function () {
+				$(this).css('color', 'yellow');
+				colorBlink('h1.main-titulo');
+			},
+			queue: true
+		});
+}
+
